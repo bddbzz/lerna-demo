f2f9f9df80ce607de95ef8a6727f028f9a172fd9
zhangz07@163.com <871013zz>
2019-11-25 12:44:50 +0800

    chore(module1): test

--- packages/lerna-module-1/lib/lerna-module-1.js
+++ packages/lerna-module-1/lib/lerna-module-1.js
@@ -1,12 +1,11 @@
-'use strict';
-const chalk = require("chalk")
-const lernaModule2 = require("@monkeyzz/lerna-module-2")
+'use strict'
+const chalk = require('chalk')
+const lernaModule2 = require('@monkeyzz/lerna-module-2')
 
-module.exports = lernaModule1;
+module.exports = lernaModule1
 
-function lernaModule1() {
-    console.log(chalk.green("module1change"))
-    lernaModule2()
+function lernaModule1 () {
+  console.log(chalk.green('module1change test'))
+  lernaModule2()
 }
 lernaModule1()
-
